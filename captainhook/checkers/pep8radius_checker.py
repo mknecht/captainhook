d3f048c605b1d02fb2899a8b5017dce1f6ea5788
Murat Knecht <murat.knecht@gmx.de>
2015-09-19 20:14:41 +0800

    pep8radius: Finishing up implementation for PR.
    Adding details to docstring and why-comments to implementation.

--- captainhook/checkers/pep8radius_checker.py
+++ captainhook/checkers/pep8radius_checker.py
@@ -12,14 +12,22 @@ NO_PEP8RADIUS_MSG = (
 
 
 def run(files, temp_folder, argstring=''):
-    "Check PEP-8 errors, but only in the diff."
+    """Check PEP-8 errors, but only in the diff.
 
+    Arguments are passed verbatim to pep8radius.
+
+    See pep8radius on GitHub: https://github.com/hayd/pep8radius
+
+    """
     try:
         import pep8radius  # NOQA
     except ImportError:
         return NO_PEP8RADIUS_MSG
 
     return bash(
-        # Argument may contain unicode, i.e. in config file name.
+        # unicode because argument string may contain unicode,
+        # i.e. in config file name.
+        # --no-color because the color reset did not work when I tried it,
+        # screwing up the colors after the diff, too.
         u'pep8radius --diff --no-color {}'.format(argstring)
     ).value()
